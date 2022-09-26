3c95df0bc012a3f74e0af5ceeb08c160f485b5ad
Emanuel Bernardi <bernardi.emanuel@gruposervices.com.br>
2022-09-26 16:38:15 -0300

    feito o execicio 3

--- exercicio3.php
+++ exercicio3.php
@@ -1,3 +1,9 @@
 <?php
 
 /*4.3.8 Prenome - Escrever um programa que lê um nome completo e mostra na tela o prenome, isto é, o primeiro nome. Suponha que o nome nunca começa com um espaço. O programa repete esses passos até que o nome lido seja uma cadeia nula (o usuário não digitou nada).*/ 
+
+
+$nome = readline('coloque o seu nome: ');
+
+$x = explode(' ', $nome);
+echo $x[0];
