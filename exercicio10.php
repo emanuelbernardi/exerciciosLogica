623978720625b257eb037f5e453dd58661f8ea54
Emanuel Bernardi <bernardi.emanuel@gruposervices.com.br>
2022-09-27 17:16:48 -0300

    feito execicio 10

---
+++ exercicio10.php
@@ -0,0 +1,27 @@
+<?php
+/*Dessa vez, seguindo a mesma base dos demais, construa:
+
+(…….)
+
+*********
+*******
+*****
+***
+* */
+$quantidade = (int)readline('Quantas linhas: ');
+  for ($f = 0; $quantidade != 0 ; $f++){
+
+  $caracter = readline('Escolha um caracter: ');
+
+  for($i = 0; $i < $quantidade; $i++) {
+    for ($e = $quantidade; $e > $i; $e--) {
+      echo $caracter.$caracter;
+    }   
+    echo "\n";
+
+  }
+  $quantidade = (int)readline('digite novamente: ');
+
+
+}
+
