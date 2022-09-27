d83f3629b3fd03e962bb06f5f4c3040bf10010e3
Emanuel Bernardi <bernardi.emanuel@gruposervices.com.br>
2022-09-27 13:21:32 -0300

    feito exercicio 5

---
+++ exercicio5.php
@@ -0,0 +1,40 @@
+<?php
+/*3.1.2 Múltipla escolha 1 - Elaborar uma questão de múltipla escolha de uma disciplina 
+que esteja cursando ou um tema de interesse, com um enunciado e cinco alternativas, 
+sendo uma correta ou incorreta. Escrever um programa que mostra a questão na tela, 
+pede a resposta correta e informa ao usuário se este acertou ou errou. */
+
+echo "Qual é o bloco econômico que o brasil faz parte atualmente";
+
+
+echo <<<EOL
+
+Alternarivas:
+1-União Europeia
+2-USMCA
+3-APEC
+4-Mercosul
+5-ASEAN
+"\n"
+EOL;
+
+$alternativa = readline('A alternativa correta é: ');
+
+switch($alternativa)
+{
+  case 1:
+    echo "Errou"."\n";
+    break;
+  case 2:
+    echo "Errou"."\n";
+    break;
+  case 3:
+    echo "Errou"."\n";
+    break;
+  case 4:
+    echo "Acertou"."\n";
+    break;
+  case 5:
+    echo "Errou"."\n";
+    break;        
+}
