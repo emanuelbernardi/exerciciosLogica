2f5f1ab92905da9e035c0eb95a6cb29ef4fa8b9f
Emanuel Bernardi <bernardi.emanuel@gruposervices.com.br>
2022-09-27 13:58:53 -0300

    feito exercicio 7

--- exercicio7.php
+++ exercicio7.php
@@ -1,2 +1,12 @@
 <?php
+/*3.2.15 Ano bissexto -
+ Um ano é bissexto se for divisível por 4 exceto os séculos, 
+ que são bissextos se forem múltiplos de 400. Escreva um programa que determina se um ano é bissexto.*/
 
+  $ano = readline('veja se é bissexto: ');
+
+if($ano % 4 == 0 && $ano % 100 != 0 || $ano % 400 == 0) {
+  echo "É bissexto";
+} else {
+  echo "Não é";
+}
