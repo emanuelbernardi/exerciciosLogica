19a46998ba3b5226b3bb052c8074a16fa0eaf8cf
Emanuel Bernardi <bernardi.emanuel@gruposervices.com.br>
2022-09-30 16:01:24 -0300

    feito 14

--- exercicio14.php
+++ exercicio14.php
@@ -1,2 +1,25 @@
 <?php
 
+$i = 0; 
+$vetor = [];
+do{
+  $numeros = readline('coloque até 20 numeros: ');
+  $novamente = readline('Quer continuar s/n: ');
+  $vetor[$i] = $numeros;
+  
+  $i++;
+} while($novamente != 'n' &&  $i < 20);
+$ordenar = readline('1 - Ordenar crescente | 2 - Decrescente: ');
+if($ordenar == '1') {
+  asort($vetor);
+  foreach($vetor as $key => $val) 
+  {
+    echo "$key = $val \n";
+  }
+} else {
+  arsort($vetor);
+  foreach($vetor as $key => $val) 
+  {
+    echo "$key = $val \n";
+  }
+}
