51d783fbf27b89d3068041ebbbf754e21da9398a
Emanuel Bernardi <bernardi.emanuel@gruposervices.com.br>
2022-10-03 14:33:10 -0300

    feat: add algumas coisas

--- exercicio17.php
+++ exercicio17.php
@@ -2,12 +2,15 @@
 /*5.2.1 Menor cadeia - 
 Escrever uma função que recebe duas cadeias de caracteres e retorna a menor
 em ordem alfabética.  */
-$cadei1 = readline('coloque algumas letras para ordenar por ordem alfabética: ');
-$cadeia2 = readline('coloque algumas letras para ordenar: ');
 
-function ordemAlfa($cadei1,$cadeia2)
+
+function ordemAlfa($var1,$var2)
 {
   
   
 
 }
+$cadeia1 = readline('coloque algumas letras para ordenar por ordem alfabética: ');
+$cadeia2 = readline('coloque algumas letras para ordenar: ');
+
+echo ordemAlfa($cadeia1,$cadeia2);
--- exercicio20.php
+++ exercicio20.php
@@ -1,2 +1,2 @@
 <?php
-/*Meu execicio autoral */
+/* Execicio autoral */
