b0a87c1d212539b701dfdd71ef969b440f2d1fbd
Emanuel Bernardi <bernardi.emanuel@gruposervices.com.br>
2022-10-03 01:36:16 -0300

    feat: 17

--- exercicio17.php
+++ exercicio17.php
@@ -2,3 +2,12 @@
 /*5.2.1 Menor cadeia - 
 Escrever uma função que recebe duas cadeias de caracteres e retorna a menor
 em ordem alfabética.  */
+$cadei1 = readline('coloque algumas letras para ordenar por ordem alfabética: ');
+$cadeia2 = readline('coloque algumas letras para ordenar: ');
+
+function ordemAlfa($cadei1,$cadeia2)
+{
+  
+  
+
+}
