be18812534afc96abcade5591db29587d0944b2f
Emanuel Bernardi <bernardi.emanuel@gruposervices.com.br>
2022-10-03 01:35:35 -0300

    feat: titulo 19

--- exercicio19.php
+++ exercicio19.php
@@ -1,5 +1,6 @@
 <?php
-/*4.6.5 Soma em vetor 2 - Elabore um programa semelhante ao anterior, exceto que, em apenas um
+/*4.6.5 Soma em vetor  - 
+Elabore um programa  que em apenas um
 vetor, soma os valores de dois campos e armazena o resultado em um terceiro campo de um
 registro. */
 
